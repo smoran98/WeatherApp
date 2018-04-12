33e6043569d86b1ce7f08f5bb853f427dc0fa537
SHANE MORAN - Student <G00338607@gmit.ie>
2018-04-12 15:37:38 +0100

    few changes made

--- UWPWeather/UWPWeather/MainPage.xaml.cs
+++ UWPWeather/UWPWeather/MainPage.xaml.cs
@@ -30,30 +30,41 @@ namespace UWPWeather
 
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            var position = await LocationManager.GetPosition();
+            try
+            {
+                var position = await LocationManager.GetPosition();
 
-            var lat = position.Coordinate.Latitude;
-            var lon = position.Coordinate.Longitude;
+                var lat = position.Coordinate.Latitude;
+                var lon = position.Coordinate.Longitude;
 
-            RootObject myWeather = 
-                await OpenWeatherMapProxy.GetWeather(
-                    lat, 
-                    lon);
+                RootObject myWeather =
+                    await OpenWeatherMapProxy.GetWeather(
+                        lat,
+                        lon);
 
-            // Schedule update
-            var uri = String.Format("http://uwpweatherservice.azurewebsites.net/?lat={0}&lon={1}", lat, lon);
+                // Schedule update
+                var uri = String.Format("http://uwpweatherservice.azurewebsites.net/?lat={0}&lon={1}", lat, lon);
 
-            var tileContent = new Uri(uri);
-            var requestedInterval = PeriodicUpdateRecurrence.HalfHour;
+                var tileContent = new Uri(uri);
+                var requestedInterval = PeriodicUpdateRecurrence.HalfHour;
 
-            var updater = TileUpdateManager.CreateTileUpdaterForApplication();
-            updater.StartPeriodicUpdate(tileContent, requestedInterval);
+                var updater = TileUpdateManager.CreateTileUpdaterForApplication();
+                updater.StartPeriodicUpdate(tileContent, requestedInterval);
 
-            string icon = String.Format("ms-appx:///Assets/Weather/{0}.png", myWeather.weather[0].icon);
-            ResultImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
-            ResultTextBlock.Text = myWeather.name + " - " + ((int)myWeather.main.temp).ToString() + " - " + myWeather.weather[0].description;
+                string icon = String.Format("ms-appx:///Assets/Weather/{0}.png", myWeather.weather[0].icon);
+                ResultImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+
+                TempTextBlock.Text = ((int)myWeather.main.temp).ToString();
+                DescriptionTextBlock.Text = myWeather.weather[0].description;
+                LocationTextBlock.Text = myWeather.name;
+            }
+            catch
+            {
+                LocationTextBlock.Text = "Unable to get weather at this time.";
+            }
+
         }
     }
 }
