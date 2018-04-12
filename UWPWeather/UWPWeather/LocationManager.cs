7ed82b02325e4ef9fc12753fbf58991be957f3cf
SHANE MORAN - Student <G00338607@gmit.ie>
2018-04-12 14:29:53 +0100

    first commit

---
+++ UWPWeather/UWPWeather/LocationManager.cs
@@ -0,0 +1,26 @@
+﻿// G00338607
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace UWPWeather
+{
+    public class LocationManager
+    {
+        public async static Task<Geoposition> GetPosition()
+        {
+            var accessStatus = await Geolocator.RequestAccessAsync();
+
+            if (accessStatus != GeolocationAccessStatus.Allowed) throw new Exception();
+
+            var geolocator = new Geolocator { DesiredAccuracyInMeters = 0 };
+
+            var position = await geolocator.GetGeopositionAsync();
+
+            return position;
+        }
+    }
+}
